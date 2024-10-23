2a5a11de2d87fefc60b7d8f109d2307dd54b1996
GoodGuyGregory <greg.witt625@gmail.com>
2024-10-23 10:24:24 -0700

    adds research pandas change to support sorting the dict

--- research_paper.py
+++ research_paper.py
@@ -1,4 +1,5 @@
 import re
+import pandas as pd
 
 class ResearchPaper():
 
@@ -10,9 +11,18 @@ class ResearchPaper():
     """
         returns the highest reference paper count
     """
-    def highest_reference_paper(self,):
-        
-        pass
+    def highest_reference_paper(self, n):
+        reference_dict = pd.DataFrame.from_dict(self.reference_papers, orient='index', columns=['reference_count'])
+        # set the index as the keys from the dictionary
+        reference_dict.reset_index(inplace=True)
+        # renames the column of the keys to 'ref_paper' in place
+        reference_dict.rename(columns={'index': 'ref_paper'}, inplace=True)
+        top_n_paper_citations = reference_dict.sort_values('reference_count', ascending=False)[:n]
+        print(f"Top {n} Research Sitations")
+        print("----------------------------------")
+        print(top_n_paper_citations[['ref_paper','reference_count']])
+
+
 
     def parse_research_papers(self):
         for page_num in range(len(self.page_length)):
@@ -23,5 +33,25 @@ class ResearchPaper():
             page_content = page.extract_text()
             matches = re.findall(reference_regex, page_content)
             if len(matches) > 0:
-
+                for match in matches:
+                    if len(match) > 2:
+                        found_nums = match.split(',')
+                        for digit in found_nums:
+                            digit = digit.strip()
+                            if int(digit) < 100:
+                                if int(digit) != 0:
+                                    if digit not in self.reference_papers:
+                                        self.reference_papers[digit] = 1
+                                    else:
+                                        # increases occurences
+                                        self.reference_papers[digit] += 1
+                    else:
+                        if int(match) < 1000:
+                            if int(match) != 0:
+                                if match not in self.reference_papers:
+                                    self.reference_papers[match] = 1
+                                else:
+                                    # increases occurences
+                                    self.reference_papers[match] += 1
+                
 
